f5549cd67b03bc5a030c561fc672ce3144ba59f6
Arthur Lee <arthur.lee945@gmail.com>
2024-02-25 20:12:50 -0600

    adding drag force

--- example/mover/main.go
+++ example/mover/main.go
@@ -36,7 +36,7 @@ func main() {
 func runCanvas(ctx *canvas.Context) {
 	ctx.SetFillStyle(color.RGBA{0x08, 0x08, 0x08, 0xff})
 
-	engine := NewMoverView()
+	engine := NewMoverView(float64(ctx.CanvasWidth()), float64(ctx.CanvasHeight()))
 
 	for {
 		select {
@@ -60,12 +60,17 @@ func httpLink(addr string) string {
 	return "http://" + addr
 }
 
-type MoverView [100]*Mover
+type MoverView struct {
+	movers [100]*Mover
+	liquid *Liquid
+}
 
-func NewMoverView() *MoverView {
-	movers := &MoverView{}
+func NewMoverView(w, h float64) *MoverView {
+	movers := &MoverView{
+		liquid: NewLiquid(0, h/2, w, h/2, 0.01),
+	}
 	for i := range 100 {
-		movers[i] = NewMover(0, 0, rand.Float64()*8+2)
+		movers.movers[i] = NewMover(0, 0, rand.Float64()*8+2)
 	}
 	return movers
 }
@@ -77,13 +82,19 @@ func (mv *MoverView) Draw(ctx *canvas.Context) {
 	ctx.Rect(0, 0, width, height)
 	ctx.Stroke()
 
+	mv.liquid.Display(ctx)
 	wind := vec.NewVec2(0.01, 0)
 
-	for _, m := range mv {
-		c := 0.01
-		friction := force.Friction(m.velocity, c, 1)
-		gravity := vec.NewVec2(0, 0.1*m.mass)
-		m.ApplyForce(friction)
+	for _, m := range mv.movers {
+		// c := 0.01
+		// friction := force.Friction(m.velocity, c, 1)
+		// m.ApplyForce(friction)
+		if m.isIniside(*mv.liquid) {
+			drag := force.Drag(m.velocity, mv.liquid.coefficient)
+			m.ApplyForce(drag)
+		}
+
+		gravity := vec.NewVec2(0, 0.01*m.mass)
 		m.ApplyForce(wind)
 		m.ApplyForce(gravity)
 
@@ -138,4 +149,32 @@ func (m *Mover) CheckEdges(x, y float64) {
 		m.velocity[1] *= -1
 		m.loc[1] = y
 	}
+}
+
+func (m *Mover) isIniside(liq Liquid) bool {
+	if m.loc[0] >= liq.loc[0] && m.loc[0] <= liq.loc[0]+liq.size[0] && m.loc[1] >= liq.loc[1] && m.loc[1] <= liq.loc[1]+liq.size[1] {
+		return true
+	}
+	return false
+}
+
+type Liquid struct {
+	loc         *vec.Vec2
+	size        *vec.Vec2
+	coefficient float64
+}
+
+func NewLiquid(x, y, width, height, coefficient float64) *Liquid {
+	return &Liquid{
+		&vec.Vec2{x, y},
+		&vec.Vec2{width, height},
+		coefficient,
+	}
+}
+func (l *Liquid) Display(ctx *canvas.Context) {
+	ctx.BeginPath()
+	ctx.Rect(l.loc[0], l.loc[1], l.size[0], l.size[1])
+	ctx.SetFillStyle(color.RGBA{0, 0, 255, 100})
+	ctx.Fill()
+	ctx.SetFillStyle(color.RGBA{0, 0, 0, 255})
 }
