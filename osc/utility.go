7c2a2770c842a7cbf1c3ac184fb11ac975c4d23b
Arthur Lee <arthur.lee945@gmail.com>
2024-02-28 21:13:02 -0600

    renamining oscillation

--- osc/utility.go
+++ osc/utility.go
@@ -2,7 +2,7 @@ package osc
 
 import "math"
 
-//Takes a degree and returns radians
+//Takes a degree and returns radians | Oscillation
 func Radian(deg float64) float64 {
 	return 2 * math.Pi * (deg / 360)
 }
